c7b568fb6178dea40165da985de92cfcccdba31c
Handsome Victor <857730556@qq.com>
2023-04-06 15:26:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import influxdb_client
 from influxdb_client.client.write_api import SYNCHRONOUS
 
 org = "kaiko"
-token = 
+token =
 url = "http://34.78.245.234:8086"
 
 client = influxdb_client.InfluxDBClient(
